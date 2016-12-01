360c22cd14f8deb11be898ad023c9d83bb65ea1d
mozartwang <mozartwang0314@gmail.com>
2016-12-01 16:35:41 +0800

    initial project

---
+++ simpledom.cpp
@@ -0,0 +1,16 @@
+// JSON simple example
+// This example does not handle errors.
+
+#include "rapidjson_own/document.h"
+#include <iostream>
+
+using namespace rapidjson;
+
+int main() {
+    // 1. Parse a JSON string into DOM.
+//    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+//    Document d;
+//    d.Parse(json);
+
+    return 0;
+}
