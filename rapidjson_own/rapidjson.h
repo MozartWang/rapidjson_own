6c1267ab6a7a5d3851213f13beed8329116d119e
mozart <mozartwang0314@gmail.com>
2016-12-01 20:39:31 +0800

    rapidjson.h file complete

--- rapidjson_own/rapidjson.h
+++ rapidjson_own/rapidjson.h
@@ -64,6 +64,78 @@ RAPIDJSON_NAMESPACE_END
 #define RAPIDJSON_ASSERT(x) assert(x)
 #endif
 
+///////////////////////////////////////////////////////////
+// Helper
+#define RAPIDJSON_MULTILINEMACRO_BEGIN do {
+#define RAPIDJSON_MULTILINEMACRO_END \
+} while((void)0, 0)
+
+///////////////////////////////////////////////////////////
+// new/delete
+#ifndef RAPIDJSON_NEW
+#define RAPIDJSON_NEW(x) new x
+#endif
+#ifndef RAPIDJSON_DELETE
+#define RAPIDJSON_DELETE(x) delete x
+#endif
+
+///////////////////////////////////////////////////////////
+//  Allocators and Encodings
+RAPIDJSON_NAMESPACE_BEGIN
+template<typename Stream>
+struct StreamTraits {
+    enum { copyOptimization = 0 };
+};
+
+template<typename Stream, typename Ch>
+inline void PutN(Stream& stream, Ch c, size_t n) {
+    for (size_t i = 0; i < n; i++)
+        stream.Put(c);
+}
+
+///////////////////////////////////////////////////////////
+// StringStream
+
+template <typename Encoding>
+struct GenericStringStream {
+    typedef typename Encoding::Ch Ch;
+
+    GenericStringStream(const Ch *src) : src_(src), head_(src) {}
+
+    Ch Peek() const { return *src_; }
+    Ch Take() { return *src_++; }
+    size_t Tell() const { return static_cast<size_t>(src_ - head_); }
+
+    Ch* PutBegin() { RAPIDJSON_ASSERT(false); return 0; }
+    void Put(Ch) { RAPIDJSON_ASSERT(false); }
+    void Flush() { RAPIDJSON_ASSERT(false); }
+    size_t PutEnd(Ch *) { RAPIDJSON_ASSERT(false); return 0; }
+
+    const Ch* src_;     //!< Current read position.
+    const Ch* head_;    //!< Original head of the string.
+};
+
+template<typename Encoding>
+struct StreamTraits<GenericStringStream<Encoding> > {
+    enum { copyOptimization = 1 };
+};
+
+typedef GenericStringStream<UTF8<> > StringStream;
+
+
+///////////////////////////////////////////////////////////
+// Type
+enum Type {
+    kNullType   = 0,
+    kFalseType  = 1,
+    kTrueType   = 2,
+    kObjectType = 3,
+    kArrayType  = 4,
+    kStringType = 5,
+    kNumberType = 6
+};
+
+RAPIDJSON_NAMESPACE_END
 
 
 #endif //RAPIDJSON_RAPIDJSON_H_
